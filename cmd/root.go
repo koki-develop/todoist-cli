2e8899fcadacad48105653b5a2fafbdb00ca66bb
koki-develop <kou.pg.0131@gmail.com>
2023-01-29 11:02:02 +0900

    remove init and description

--- cmd/root.go
+++ cmd/root.go
@@ -7,14 +7,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "todoist",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use: "todoist",
 }
 
 func Execute() {
@@ -22,8 +15,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
