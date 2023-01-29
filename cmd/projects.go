a0b8f49a1c21e133ac26fc8b5dc6762a296876ad
koki-develop <kou.pg.0131@gmail.com>
2023-01-29 11:49:19 +0900

    print projects table

--- cmd/projects.go
+++ cmd/projects.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/koki-develop/todoist/pkg/renderer"
+	"github.com/koki-develop/todoist/pkg/rest"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +15,24 @@ var projectsCmd = &cobra.Command{
 
 var projectsListCmd = &cobra.Command{
 	Use: "list",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cl := rest.New(&rest.Config{Token: os.Getenv("TODOIST_API_TOKEN")})
+		rdr := renderer.New(renderer.FormatTable)
+
+		projs, err := cl.ListProjects()
+		if err != nil {
+			return err
+		}
+
+		o, err := rdr.Render(projs)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(o)
+
+		return nil
+	},
 }
 
 func init() {
