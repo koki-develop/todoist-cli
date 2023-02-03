01ad992b890932b20c00b2d0021d51ded879ce27
koki-develop <kou.pg.0131@gmail.com>
2023-02-03 20:36:29 +0900

    implement unit test for todoistapi.Client.GetProject()

--- pkg/todoistapi/projects_test.go
+++ pkg/todoistapi/projects_test.go
@@ -18,12 +18,12 @@ func TestClient_ListProjects(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			resp:   `[{"id": 1, "name": "PROJECT_1"}, {"id": 2, "name": "PROJECT_2"}, {"id": 3, "name": "PROJECT_3"}]`,
+			resp:   `[{"id": "1", "name": "PROJECT_1"}, {"id": "2", "name": "PROJECT_2"}, {"id": "3", "name": "PROJECT_3"}]`,
 			status: 200,
 			want: models.Projects{
-				{"id": float64(1), "name": "PROJECT_1"},
-				{"id": float64(2), "name": "PROJECT_2"},
-				{"id": float64(3), "name": "PROJECT_3"},
+				{"id": "1", "name": "PROJECT_1"},
+				{"id": "2", "name": "PROJECT_2"},
+				{"id": "3", "name": "PROJECT_3"},
 			},
 			wantErr: false,
 		},
@@ -58,6 +58,66 @@ func TestClient_ListProjects(t *testing.T) {
 	}
 }
 
+func TestClient_GetProject(t *testing.T) {
+	tests := []struct {
+		id      string
+		resp    string
+		status  int
+		want    models.Project
+		wantErr bool
+	}{
+		{
+			id:      "1",
+			resp:    `{"id": "1", "name": "PROJECT"}`,
+			status:  200,
+			want:    models.Project{"id": "1", "name": "PROJECT"},
+			wantErr: false,
+		},
+		{
+			id:      "1",
+			resp:    "ERROR_RESPONSE",
+			status:  400,
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			id:      "1",
+			resp:    "ERROR_RESPONSE",
+			status:  500,
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			cl, m := newClientWithMock(t)
+
+			m.mockHTTP(t, &mockHTTPConfig{
+				Request: &mockHTTPConfigRequest{
+					URL:    fmt.Sprintf("https://api.todoist.com/rest/v2/projects/%s", tt.id),
+					Method: http.MethodGet,
+					Headers: map[string]string{
+						"Authorization": "Bearer TODOIST_API_TOKEN",
+						"Content-Type":  "application/json",
+					},
+				},
+				Response: &mockHTTPConfigResponse{
+					Status: tt.status,
+					Body:   tt.resp,
+				},
+			})
+
+			proj, err := cl.GetProject(tt.id)
+			if tt.wantErr {
+				assert.EqualError(t, err, tt.resp)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.want, proj)
+		})
+	}
+}
+
 func TestClient_CreateProject(t *testing.T) {
 	tests := []struct {
 		p       *CreateProjectParameters
@@ -70,9 +130,9 @@ func TestClient_CreateProject(t *testing.T) {
 		{
 			p:       &CreateProjectParameters{Name: util.Ptr("PROJECT")},
 			req:     `{"name":"PROJECT"}`,
-			resp:    `{"id": 1, "name": "PROJECT"}`,
+			resp:    `{"id": "1", "name": "PROJECT"}`,
 			status:  201,
-			want:    models.Project{"id": float64(1), "name": "PROJECT"},
+			want:    models.Project{"id": "1", "name": "PROJECT"},
 			wantErr: false,
 		},
 		{
@@ -84,9 +144,9 @@ func TestClient_CreateProject(t *testing.T) {
 				ViewStyle:  util.Ptr("VIEW_STYLE"),
 			},
 			req:     `{"name":"PROJECT","parent_id":"PARENT_ID","color":"COLOR","is_favorite":true,"view_style":"VIEW_STYLE"}`,
-			resp:    `{"id": 1, "name": "PROJECT"}`,
+			resp:    `{"id": "1", "name": "PROJECT"}`,
 			status:  201,
-			want:    models.Project{"id": float64(1), "name": "PROJECT"},
+			want:    models.Project{"id": "1", "name": "PROJECT"},
 			wantErr: false,
 		},
 		{
