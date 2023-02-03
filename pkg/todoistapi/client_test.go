9c5b65c88f8aee8ac9dffb1dcaad0786b7b050dd
koki-develop <kou.pg.0131@gmail.com>
2023-02-03 20:20:51 +0900

    implement unit test for todoistapi.Client.ListProjects, CreateProject

--- pkg/todoistapi/client_test.go
+++ pkg/todoistapi/client_test.go
@@ -1,11 +1,66 @@
 package todoistapi
 
 import (
+	"bytes"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+func newClientWithMock(t *testing.T) (*Client, *mockHttpAPI) {
+	m := newMockHttpAPI(t)
+	cl := &Client{token: "TODOIST_API_TOKEN", httpAPI: m}
+
+	return cl, m
+}
+
+type mockHTTPConfig struct {
+	Request  *mockHTTPConfigRequest
+	Response *mockHTTPConfigResponse
+}
+
+type mockHTTPConfigRequest struct {
+	URL     string
+	Method  string
+	Body    string
+	Headers map[string]string
+}
+type mockHTTPConfigResponse struct {
+	Status int
+	Body   string
+}
+
+func (m *mockHttpAPI) mockHTTP(t *testing.T, cfg *mockHTTPConfig) {
+	var b io.Reader = nil
+	if cfg.Request.Body != "" {
+		b = bytes.NewReader([]byte(cfg.Request.Body))
+	}
+
+	req, _ := http.NewRequest(cfg.Request.Method, cfg.Request.URL, b)
+	for k, v := range cfg.Request.Headers {
+		req.Header.Set(k, v)
+	}
+
+	resp := &http.Response{
+		StatusCode: cfg.Response.Status,
+		Body:       io.NopCloser(strings.NewReader(cfg.Response.Body)),
+	}
+
+	m.On("Do", mock.Anything).Return(resp, nil).Run(func(args mock.Arguments) {
+		req := args.Get(0).(*http.Request)
+		assert.Equal(t, cfg.Request.URL, req.URL.String())
+		assert.Equal(t, cfg.Request.Method, req.Method)
+		if cfg.Request.Body != "" {
+			b, _ := req.GetBody()
+			buf, _ := io.ReadAll(b)
+			assert.Equal(t, cfg.Request.Body, string(buf))
+		}
+	})
+}
 
 func TestNew(t *testing.T) {
 	cfg := &Config{Token: "TODOIST_API_TOKEN"}
---
+++ pkg/todoistapi/projects_test.go
@@ -0,0 +1,135 @@
+package todoistapi
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/koki-develop/todoist-cli/pkg/models"
+	"github.com/koki-develop/todoist-cli/pkg/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_ListProjects(t *testing.T) {
+	tests := []struct {
+		resp    string
+		status  int
+		want    models.Projects
+		wantErr bool
+	}{
+		{
+			resp:   `[{"id": 1, "name": "PROJECT_1"}, {"id": 2, "name": "PROJECT_2"}, {"id": 3, "name": "PROJECT_3"}]`,
+			status: 200,
+			want: models.Projects{
+				{"id": float64(1), "name": "PROJECT_1"},
+				{"id": float64(2), "name": "PROJECT_2"},
+				{"id": float64(3), "name": "PROJECT_3"},
+			},
+			wantErr: false,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			cl, m := newClientWithMock(t)
+
+			m.mockHTTP(t, &mockHTTPConfig{
+				Request: &mockHTTPConfigRequest{
+					URL:    "https://api.todoist.com/rest/v2/projects",
+					Method: http.MethodGet,
+					Headers: map[string]string{
+						"Authorization": "Bearer TODOIST_API_TOKEN",
+						"Content-Type":  "application/json",
+					},
+				},
+				Response: &mockHTTPConfigResponse{
+					Status: 200,
+					Body:   tt.resp,
+				},
+			})
+
+			got, err := cl.ListProjects()
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestClient_CreateProject(t *testing.T) {
+	tests := []struct {
+		p       *CreateProjectParameters
+		req     string
+		resp    string
+		status  int
+		want    models.Project
+		wantErr bool
+	}{
+		{
+			p:       &CreateProjectParameters{Name: util.Ptr("PROJECT")},
+			req:     `{"name":"PROJECT"}`,
+			resp:    `{"id": 1, "name": "PROJECT"}`,
+			status:  201,
+			want:    models.Project{"id": float64(1), "name": "PROJECT"},
+			wantErr: false,
+		},
+		{
+			p: &CreateProjectParameters{
+				Name:       util.Ptr("PROJECT"),
+				ParentID:   util.Ptr("PARENT_ID"),
+				Color:      util.Ptr("COLOR"),
+				IsFavorite: util.Ptr(true),
+				ViewStyle:  util.Ptr("VIEW_STYLE"),
+			},
+			req:     `{"name":"PROJECT","parent_id":"PARENT_ID","color":"COLOR","is_favorite":true,"view_style":"VIEW_STYLE"}`,
+			resp:    `{"id": 1, "name": "PROJECT"}`,
+			status:  201,
+			want:    models.Project{"id": float64(1), "name": "PROJECT"},
+			wantErr: false,
+		},
+		{
+			p:       &CreateProjectParameters{Name: util.Ptr("PROJECT")},
+			req:     `{"name":"PROJECT"}`,
+			resp:    "ERROR_RESPONSE",
+			status:  400,
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			p:       &CreateProjectParameters{Name: util.Ptr("PROJECT")},
+			req:     `{"name":"PROJECT"}`,
+			resp:    "ERROR_RESPONSE",
+			status:  500,
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			cl, m := newClientWithMock(t)
+
+			m.mockHTTP(t, &mockHTTPConfig{
+				Request: &mockHTTPConfigRequest{
+					URL:    "https://api.todoist.com/rest/v2/projects",
+					Method: http.MethodPost,
+					Body:   tt.req,
+					Headers: map[string]string{
+						"Authorization": "Bearer TODOIST_API_TOKEN",
+						"Content-Type":  "application/json",
+					},
+				},
+				Response: &mockHTTPConfigResponse{Status: tt.status, Body: tt.resp},
+			})
+
+			got, err := cl.CreateProject(tt.p)
+			if tt.wantErr {
+				assert.EqualError(t, err, tt.resp)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
