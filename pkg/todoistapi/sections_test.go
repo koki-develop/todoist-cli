fcc7449f8d9f6f6fe37cb360b14c1073942bfaf6
koki-develop <kou.pg.0131@gmail.com>
2023-02-04 09:11:40 +0900

    implement unit test for todoistapi.Client.ListSections()

---
+++ pkg/todoistapi/sections_test.go
@@ -0,0 +1,83 @@
+package todoistapi
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/koki-develop/todoist-cli/pkg/models"
+	"github.com/koki-develop/todoist-cli/pkg/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_ListSections(t *testing.T) {
+	tests := []struct {
+		p           *ListSectionsParameters
+		expectedURL string
+		resp        string
+		status      int
+		want        models.Sections
+		wantErr     bool
+	}{
+		{
+			p:           &ListSectionsParameters{},
+			expectedURL: "https://api.todoist.com/rest/v2/sections",
+			resp:        `[{"id": "1", "name": "SECTION_1"}, {"id": "2", "name": "SECTION_2"}, {"id": "3", "name": "SECTION_3"}]`,
+			status:      200,
+			want:        models.Sections{{"id": "1", "name": "SECTION_1"}, {"id": "2", "name": "SECTION_2"}, {"id": "3", "name": "SECTION_3"}},
+			wantErr:     false,
+		},
+		{
+			p:           &ListSectionsParameters{ProjectID: util.Ptr("PROJECT_ID")},
+			expectedURL: "https://api.todoist.com/rest/v2/sections?project_id=PROJECT_ID",
+			resp:        `[{"id": "1", "name": "SECTION_1"}, {"id": "2", "name": "SECTION_2"}, {"id": "3", "name": "SECTION_3"}]`,
+			status:      200,
+			want:        models.Sections{{"id": "1", "name": "SECTION_1"}, {"id": "2", "name": "SECTION_2"}, {"id": "3", "name": "SECTION_3"}},
+			wantErr:     false,
+		},
+		{
+			p:           &ListSectionsParameters{},
+			expectedURL: "https://api.todoist.com/rest/v2/sections",
+			resp:        "ERROR_RESPONSE",
+			status:      400,
+			want:        nil,
+			wantErr:     true,
+		},
+		{
+			p:           &ListSectionsParameters{},
+			expectedURL: "https://api.todoist.com/rest/v2/sections",
+			resp:        "ERROR_RESPONSE",
+			status:      500,
+			want:        nil,
+			wantErr:     true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			cl, m := newClientWithMock(t)
+
+			m.mockHTTP(t, &mockHTTPConfig{
+				Request: &mockHTTPConfigRequest{
+					URL:    tt.expectedURL,
+					Method: http.MethodGet,
+					Headers: map[string]string{
+						"Authorization": "Bearer TODOIST_API_TOKEN",
+						"Content-Type":  "application/json",
+					},
+				},
+				Response: &mockHTTPConfigResponse{
+					Status: tt.status,
+					Body:   tt.resp,
+				},
+			})
+
+			got, err := cl.ListSections(tt.p)
+			if tt.wantErr {
+				assert.EqualError(t, err, tt.resp)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
